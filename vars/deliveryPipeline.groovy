901402f0d86e4f9bd75527f6a20bc7d7d40becea
venu0336 <venu.rachakonda@rackspace.com>
2020-07-07 17:39:55 -0500

    docker sock

--- vars/deliveryPipeline.groovy
+++ vars/deliveryPipeline.groovy
@@ -4,7 +4,7 @@ def call(Map pipelineParams) {
        agent {
            docker {
                image 'maven:3-alpine'
-               args '-v /root/.m2:/root/.m2'
+               args '-v /root/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock'
            }
        }
        stages {
